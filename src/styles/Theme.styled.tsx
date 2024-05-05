73a154e621999642127ceeb01b14b9c2e6c93672
Yury Samsonenko <cant09y@gmail.com>
2024-05-05 15:53:11 +0300

    feat: add images and global styles

---
+++ src/styles/Theme.styled.tsx
@@ -0,0 +1,11 @@
+export const MyTheme = {
+  colors: {
+    primary: '#ffb400',
+    dark: {
+      primary: '#2b2b2b',
+      secondary: '#767676',
+      support: '#f0f0f6',
+    },
+    white: '#ffffff',
+  },
+};
